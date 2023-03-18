2fc32e0c095a80b1804f00ad83468e36065ca451
JarneBukkes <jarne.budts@hotmail.com>
2023-03-18 19:45:27 +0100

    install PostCSS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: 'eslint:recommended',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "rules": {
-    }
+    rules: {},
 }
